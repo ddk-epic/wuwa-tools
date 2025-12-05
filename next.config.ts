c5162c2085d7b230e12b2a496ed9a3c072c8bb82
ddk <dattimio95@gmail.com>
2025-12-05 20:21:34 +0100

    whitelist image links

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.imgur.com",
+        port: "",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
